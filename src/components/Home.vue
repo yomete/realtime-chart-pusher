baad2bed61988d8cf3090d8217eb5632f598e3f5
yomete <iamlese@yahoo.com>
2017-06-15 00:24:38 +0100

    removed date bug

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,11 +13,15 @@
           <h4>Add New Entry</h4>
           <div class="form-group">
             <label>Expenses</label>
-            <input class="form-control" placeholder="How much did you spend today?" type="number" v-model="expenseamount" required>
+            <input class="form-control" placeholder="How much did you spend?" type="number" v-model="expenseamount" required>
           </div>
           <div class="form-group">
             <label>Income</label>
-            <input class="form-control" placeholder="How much did you earn today?" type="number" v-model="incomeamount" required>
+            <input class="form-control" placeholder="How much did you earn?" type="number" v-model="incomeamount" required>
+          </div>
+          <div class="form-group">
+            <label>Date</label>
+            <input class="form-control" placeholder="Date" type="date" v-model="entrydate" required>
           </div>
           <div class="form-group">
             <button class="btn btn-primary">Add New Entry</button>
@@ -34,7 +38,7 @@
   import Pusher from 'pusher-js'
   import LineChart from '@/components/LineChart'
 
-  const socket = new Pusher('APP_KEY', {
+  const socket = new Pusher('3e6b0e8f2442b34330b7', {
     cluster: 'eu',
     encrypted: true
   })
@@ -50,7 +54,8 @@
         date: null,
         expenseamount: null,
         incomeamount: null,
-        datacollection: null
+        datacollection: null,
+        entrydate: null
       }
     },
     created () {
@@ -64,9 +69,8 @@
       fillData () {
         axios.get('/finances')
           .then(response => {
-            console.log(response.data)
+            console.log(response.data.data)
             let results = response.data.data
-
             let dateresult = results.map(a => a.date)
             let expenseresult = results.map(a => a.expense)
             let incomeresult = results.map(a => a.income)
@@ -98,7 +102,8 @@
       addExpenses () {
         let expense = this.expenseamount
         let income = this.incomeamount
-        let today = moment().format('MMMM Do YYYY')
+        let today = moment(this.entrydate).format('MMMM Do YYYY')
+
         axios.post('/expense/add', {
           expense: expense,
           income: income,
@@ -107,6 +112,7 @@
           .then(response => {
             this.expenseamount = ''
             this.incomeamount = ''
+            this.entrydate = ''
             channel.bind('new-expense', function (data) {
               let results = data.newExpense.data
 
@@ -141,30 +147,28 @@
             console.log(error)
           })
       },
+
       fetchData () {
-        let self = this
-        channel.bind('new-expense', function (data) {
-          let results = data.newExpense.data
-          let dateresult = results.map(a => a.date)
-          let expenseresult = results.map(a => a.expense)
-          let incomeresult = results.map(a => a.income)
-
-          self.expense = expenseresult
-          self.income = incomeresult
-          self.date = dateresult
-
-          self.datacollection = {
-            labels: self.date,
+        channel.bind('new-expense', data => {
+          let _results = data.newExpense.data
+          let dateresult = _results.map(a => a.date)
+          let expenseresult = _results.map(a => a.expense)
+          let incomeresult = _results.map(a => a.income)
+          this.expense = expenseresult
+          this.income = incomeresult
+          this.date = dateresult
+          this.datacollection = {
+            labels: this.date,
             datasets: [
               {
                 label: 'Expense Charts',
                 backgroundColor: '#f87979',
-                data: self.expense
+                data: this.expense
               },
               {
                 label: 'Income Charts',
                 backgroundColor: '#5bf8bf',
-                data: self.income
+                data: this.income
               }
             ]
           }
